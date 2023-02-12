47c86b5b60cc79df327bcc2e4007909b1a0ac3b2
Mohammad Issawi <2000issawi@gmail.com>
2023-02-12 04:01:01 +0300

    Refactor listen to client on server function

--- server/client.go
+++ server/client.go
@@ -24,9 +24,9 @@ SOFTWARE.
 package server
 
 import (
-    "go-pong/client"
-    "log"
-    "net"
+	"go-pong/client"
+	"log"
+	"net"
 )
 
 type clientStr struct {
@@ -34,31 +34,32 @@ type clientStr struct {
 	address net.Addr
 }
 
-func listenToClient(){
-    var buffer [64]byte
-    for{
-        n,addr,err:=udpServerHandle.ReadFrom(buffer[:])
-        if addr==nil{
-            continue
-        }
-        if err!=nil{
-            log.Print("Failed to read input from ",addr.String() ,"error:",err.Error())
-            continue
-        }
-        if n==1{
-            var dir int
-            if buffer[0]==0{
-                dir=-1
-            }else{
-                dir=1
-            }
-            playersMutex[gameLobby.clients[addr.String()].ID].Lock()
-            players[gameLobby.clients[addr.String()].ID].Move(float64(dir)*30, deltaTime)
-            playersMutex[gameLobby.clients[addr.String()].ID].Unlock()
-        }
-        if string(buffer[:n])==client.CLOSE_MSG{
-            gameLobby.disconnected<-gameLobby.clients[addr.String()]
-            return
-        }
-    }
+func listenToClient() {
+	var buffer [64]byte
+	for {
+		_, addr, err := udpServerHandle.ReadFrom(buffer[:])
+		if addr == nil {
+			continue
+		}
+		if err != nil {
+			log.Print("Failed to read input from ", addr.String(), "error:", err.Error())
+			continue
+		}
+		switch buffer[0] {
+		case client.CLOSE_MSG:
+			closeChannel <- true
+		case client.DATA_MSG:
+			var dir int
+			if buffer[1] == 0 {
+				dir = -1
+			} else {
+				dir = 1
+			}
+			playersMutex[gameLobby.clients[addr.String()].ID].Lock()
+			players[gameLobby.clients[addr.String()].ID].Move(float64(dir)*30, deltaTime)
+			playersMutex[gameLobby.clients[addr.String()].ID].Unlock()
+		default:
+			log.Print("Unknown message type recieved from ", addr.String(), " message type:", buffer[0])
+		}
+	}
 }
